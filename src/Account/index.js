1c7fe6ed3d5c5e868ffc59ff965095f207795078
John Oro <johnmichaeloro@gmail.com>
2019-05-05 11:19:28 -0600

    finished

--- src/Account/index.js
+++ src/Account/index.js
@@ -21,16 +21,24 @@ class Account extends Component {
     e.preventDefault();
     const amount = parseInt(this.inputBox.value);
     const newBalance = this.state.balance - amount;
-    this.setState({
-      balance: newBalance
-    })
-    this.inputBox.value = '';
+    if(newBalance >= 0) {
+      this.setState({
+        balance: newBalance
+      })
+      this.inputBox.value = '';
+    } else {
+      this.inputBox.value = "Cannot complete."
+    }
   }
   render() {
+    let balanceClass = 'balance';
+    if(this.state.balance === 0) {
+      balanceClass += ' zero';
+    }
     return (
       <div className="account">
         <h2>{this.props.name}</h2>
-        <div className="balance">{this.state.balance}</div>
+        <div className={balanceClass}>{this.state.balance}</div>
         <input type="text" placeholder="enter an amount" ref={(input) => this.inputBox = input} />
         <input type="button" value="Deposit" onClick={this.handleDepositClick} />
         <input type="button" value="Withdraw" onClick={this.handleWithdrawClick} />
